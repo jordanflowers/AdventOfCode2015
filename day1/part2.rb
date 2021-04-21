ecf0d2e7cfe73da5c9e6eaf06b70c9759f2f1437
Jordan <jordanflowers00@gmail.com>
2021-04-20 21:01:42 -0700

    part 2 complete

--- day1/part2.rb
+++ day1/part2.rb
@@ -1,10 +1,15 @@
 data = File.read('input.txt')
 elevator = 0
+count = 0
 data.split("").each do |c|
+    count += 1
     if c == '('
         elevator += 1
     elsif c == ')'
         elevator -= 1
+        if elevator == -1
+            puts count 
+        end
     end
 end
 puts "floor: %s" % [elevator]
